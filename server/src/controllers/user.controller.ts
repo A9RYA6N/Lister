b35a175c60750e8e0d117ea2ca421a699b6deb07
A9RYA6N <aryanburnwal8@gmail.com>
2025-07-13 11:00:36 +0530

    Added jwt tokenisation in login and prototyped few functions for the future

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,16 +1,27 @@
 import { Response, Request } from "express"
 import {client} from '../database/db'
 import bcrypt, { hash } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import { Result } from "pg"
+
+const generateAccessToken=(id: number)=>{
+    return jwt.sign({id: id}, process.env.JWT_SECRET||'mhasdnhgvasdmhgv')
+}
+
 const login=async(req: Request, res: Response)=>{
     const {email, password}=req.body
     try {
-        const result= await client.query('select email, password from users where email=$1', [email])
+        const result= await client.query('select id, email, password from users where email=$1', [email])
         if(result){
             const savedPassword=result.rows[0]['password']
             const isPwdMatching=await bcrypt.compare(password, savedPassword)
-            isPwdMatching ? res.status(200).json({success:true, data:result.rows[0]})
-                :res.status(400).json({success:false, message:"Wrong Password"})
+            if(isPwdMatching){
+                const token=generateAccessToken(result.rows[0]['id'])
+                res.status(200).json({success:true, data:token})
+            }
+            else{
+                res.status(400).json({success:false, message:"Wrong Password"})
+            }
         }
         else{
             res.status(401).json({success:false, message:"User doesnt exist"})
@@ -20,6 +31,7 @@ const login=async(req: Request, res: Response)=>{
         res.status(500).json({success:false, message:"Error logging in "+error})
     }
 }
+
 const signUp=async(req: Request, res: Response)=>{
     const {name, email, password}=req.body
     const salt=await bcrypt.genSalt(10)
@@ -40,4 +52,21 @@ const signUp=async(req: Request, res: Response)=>{
         }
     }
 }
+
+const getUser=async(req: Request, res: Response)=>{
+
+}
+
+const deleteUser=async(req: Request, res: Response)=>{
+
+}
+
+const logout=async(req: Request, res: Response)=>{
+
+}
+
+const editUser=async(req: Request, res: Response)=>{
+
+}
+
 export {login, signUp}
